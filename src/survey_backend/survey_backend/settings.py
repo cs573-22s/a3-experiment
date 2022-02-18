59d4fb5dc76b30a18028a477dad601fb7aa4610b
James Plante <jplante@wpi.edu>
2022-02-18 09:47:45 -0500

    More initial frontend

--- src/survey_backend/survey_backend/settings.py
+++ src/survey_backend/survey_backend/settings.py
@@ -9,18 +9,15 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from dotenv import dotenv_values
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SECRET_KEY = dotenv_values(".env")["SECRET_KEY"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x*80o(nbi!o-in3*46^q95kgk5m1x#c-ijt@l&w*1i!_un0$g0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
