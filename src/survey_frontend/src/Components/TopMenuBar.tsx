25c4dc52627806102dab5498666f48cfd3237987
James Plante <jplante@wpi.edu>
2022-02-19 20:43:53 -0500

    Initial home menu

---
+++ src/survey_frontend/src/Components/TopMenuBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+
+export default function ButtonAppBar () {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            CS573 Survey
+          </Typography>
+          <Button color="inherit">About</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
