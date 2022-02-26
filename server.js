ff167ac709f44ba4973f7b92ce37f35f5499940e
Hilson Shrestha <hilsonshrestha@gmail.com>
2022-02-25 21:59:35 -0500

    Connect and add user response to database

--- server.js
+++ server.js
@@ -1,46 +1,78 @@
-const port = 3001,
-    express = require('express'),
-    morgan = require('morgan'),
-    app = express();
-require('dotenv').config();
+require("dotenv").config();
 
-const {response, request} = require("express");
+const { response, request } = require("express");
+const pool = require("./db");
+const bodyParser = require("body-parser");
 
+const port = 3001;
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+// async function retrieveData() {
+//   try {
+//     const res = await pool.query("SELECT * FROM response");
+//     console.log(res.rows);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// retrieveData();
 
 app.listen(process.env.PORT || port);
-
+app.use(bodyParser.json());
 
 //register view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // middleware & static files
-app.use(express.urlencoded({extended: true}));
-app.use(morgan('dev'));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 // serve up static files in the directory public
-app.use(express.static('public'));
-app.use((req,res,next) => {
-    res.locals.path = req.path;
-    next();
+app.use(express.static("public"));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
 });
 
-
-app.get('/', (req,res) => {
-    res.redirect('/index');
-})
-
-app.get('/index', async (req, res) => {
-
-    res.render('index');
-
+app.get("/", (req, res) => {
+  res.redirect("/index");
 });
 
-app.get('/experiment', async (req, res) => {
-    res.render('experiment');
+app.get("/index", async (req, res) => {
+  res.render("index");
+});
+
+app.get("/experiment", async (req, res) => {
+  res.render("experiment");
+});
+
+app.post("/api/experiments/:questionNumber", async (req, res) => {
+  const {questionNumber} = req.params;
+  const {trueAnswer, userAnswer, user} = req.body;
+
+  try {
+    const res = await pool.query(
+      "INSERT INTO response (question_no, actual_answer, user_answer, user_id) VALUES ($1, $2, $3, $4)",
+      [
+        req.params.questionNumber,
+        req.body.trueAnswer,
+        req.body.userAnswer,
+        req.body.user,
+      ]
+    );
+    console.log(res.rows);
+  } catch (error) {
+    console.error(error);
+  }
+
+  res.setHeader("Content-Type", "application/json");
+  res.status(201).end(JSON.stringify({ msg: "ok" }));
 });
 
 // 404 page
-app.use((req,res) => {
-    res.status(404).render('404',{title: '404'})
-})
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
