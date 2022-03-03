5c598753136f1dee7b88e2556eb063c06469f0d4
alpecorale <alpecorale@wpi.edu>
2022-03-03 02:26:50 -0500

    idk

---
+++ models/responseModel.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const responseSchema = new Schema({
+    userId:{
+        type:String,
+        required: true
+    },
+    test1: {
+        type: [],
+        required: true
+    },
+    test2: {
+        type: [],
+        required: true
+    },
+    test3: {
+        type: [],
+        required: true
+    },
+    test4: {
+        type: [],
+        required: true
+    },
+    test5: {
+        type: [],
+        required: true
+    },
+    test6: {
+        type: [],
+        required: true
+    },
+    test7: {
+        type: [],
+        required: true
+    },
+    test8: {
+        type: [],
+        required: true
+    },
+    test9: {
+        type: [],
+        required: true
+    },
+    test10: {
+        type: [],
+        required: true
+    },
+    test11: {
+        type: [],
+        required: true
+    },
+    test12: {
+        type: [],
+        required: true
+    }
+}, {timestamps: true });
+
+const ResponseEntry = mongoose.model('response', responseSchema);
+module.exports = ResponseEntry;
--- server.js
+++ server.js
@@ -1,24 +1,21 @@
 require("dotenv").config();
 
 const { response, request } = require("express");
-const pool = require("./db");
 const bodyParser = require("body-parser");
-
+const mongoose = require('mongoose')
+const ResponseEntry = require('./models/responseModel.js')
 const port = 3001;
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
 
-// async function retrieveData() {
-//   try {
-//     const res = await pool.query("SELECT * FROM response");
-//     console.log(res.rows);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
 
-// retrieveData();
+const uri = 'mongodb+srv://'+process.env.ACCOUNT+':'+process.env.PASS+'@'+process.env.HOST
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(result => app.listen(process.env.PORT || port))
+    .catch(err => console.log(err));
+
 
 app.listen(process.env.PORT || port);
 app.use(bodyParser.json());
@@ -37,36 +34,63 @@ app.use((req, res, next) => {
   next();
 });
 
+app.post('/postResponse', async (req, res) => {
+  const response = new ResponseEntry({
+    userId: req.body.userId,
+    test1: req.body.test1,
+    test2: req.body.test2,
+    test3: req.body.test3,
+    test4: req.body.test4,
+    test5: req.body.test5,
+    test6: req.body.test6,
+    test7: req.body.test7,
+    test8: req.body.test8,
+    test9: req.body.test9,
+    test10: req.body.test10,
+    test11: req.body.test11,
+    test12: req.body.test12
+  })
+  response.save()
+      .then(result => {
+        res.send(result);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  res.render('start')
+  return;
+})
+
+app.get('/returnResponses', (req, res) => {
+  ResponseEntry.find()
+      .then(result => {
+        /*res.json({
+          answers: result
+        })*/
+        res.send(result);
+      })
+})
+
+
 app.get("/", (req, res) => {
-  res.redirect("/index");
+  res.redirect("/start");
 });
+
+app.get("/start", async (req, res) => {
+  res.render("start");
+});
+
+app.post("/test1", async (req, res) => {
+  res.render("test1", {userId: req.body.userId});
+});
+
+app.post("/test2", bodyParser.json(), async (req, res) => {
+  res.redirect("test2", {userId: req.body.userId, test1: req.body.input1});
+});
+
 
 app.get("/index", async (req, res) => {
-  res.render("index");
-});
-
-app.get("/experiment", async (req, res) => {
-  res.render("experiment", { datasets: [[1, 2, 3, 4, 5]], answers: [10, 20] });
-});
-
-app.post("/api/experiments/:questionNumber", async (req, res) => {
-  const { questionNumber } = req.params;
-  const { trueAnswer, userAnswer, user } = req.body;
-
-  try {
-    const res = await pool.query(
-      "INSERT INTO response (question_no, true_answer, user_answer, user_id) VALUES ($1, $2, $3, $4)",
-      [questionNumber, trueAnswer, userAnswer, user]
-    );
-    console.log(res.rows);
-  } catch (error) {
-    console.error(error);
-  }
-
-  res.setHeader("Content-Type", "application/json");
-  res
-    .status(201)
-    .end(JSON.stringify({ msg: "Response saved successfully", status: 201 }));
+  res.redirect('/start')
 });
 
 // 404 page
