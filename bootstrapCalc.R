56cf1f27f03fe2a93557cc9cac146d8330276611
mnorales <48488377+mnorales@users.noreply.github.com>
2022-03-04 21:15:20 -0500

    Update bootstrapCalc.R

--- bootstrapCalc.R
+++ bootstrapCalc.R
@@ -1,45 +1,35 @@
 # https://www.geeksforgeeks.org/bootstrap-confidence-interval-with-r-programming/
+# https://cran.r-project.org/web/packages/superb/superb.pdf
 
 library(boot)
-library(ggplot2)
+library(tidyverse)
+library(superb)
 
 # load result data
 # 1 = Yes; 0 = No
 
 data<-read.csv(file = 'expResults.csv', header = TRUE) #read data 
 
-x<-data[,c("Pie.chart","Bar.chart","Scatter.plot")] #relevant columns
+newData<-data[,c("Pie.chart","Bar.chart","Scatter.plot")] #relevant columns
 
-i<-nrow(x) #count number of rows for re-sampling 
+i<-nrow(newData) #count number of rows for re-sampling 
 
-boot.mean<-function(x,i){
-  boot.mean<-mean(x[i])} #bootstrapping function to get the mean
+boot.mean<-function(newData,i){
+  boot.mean<-mean(newData[i])} #bootstrapping function to get the mean
 
 #bootstrap all columns
 #gets bootstrap mean and confidence interval 
-apply(x,2,function(y){ 
+apply(newData,2,function(y){ 
   b<-boot(y,boot.mean,R=50000); 
   c(mean(b$t),boot.ci(b,type="perc", conf=0.95)$percent[4:5])
 })
 
+# plotting bootstrap
+dta <- data.frame(plot_type = c("Pie Chart","Bar Chart","Scatter Plot"), 
+                  center= c(0.9161,0.4996,0.3335), 
+                  lower = c(0.1661,0.2496,0.2502),
+                  upper = c(0.0839,0.2504,0.2498))
 
-
-
-#Other Attempt
-data<-read.csv(file = 'expResults.csv', header = TRUE) 
-correlation <- function(data, index)
-{
-  df <- data[index, ]
-  # Find the spear man correlation between the value columns of data set
-  c(cor(df[, 2], df[,3], method = 'spearman'))
-}
-
-# bootstrap w/ data set and rounds
-bootstrap <- boot(data, correlation, R = 100)
-bootstrap
-
-# Plot sampling
-plot(bootstrap)
-
-# Confidence Intervals
-boot.ci(boot.out = bootstrap, type = c("norm","basic","perc","bca"))
+# plot of error bars
+ggplot(dta, aes_string(ymin="center-lower", ymax="center+upper", x = "plot_type" ) ) +
+  geom_superberrorbar()
