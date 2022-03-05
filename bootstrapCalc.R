c05824c9ddcd679d7e1b5190b289103ae097bf15
mnorales <48488377+mnorales@users.noreply.github.com>
2022-03-04 20:07:32 -0500

    Update bootstrapCalc.R

--- bootstrapCalc.R
+++ bootstrapCalc.R
@@ -5,16 +5,37 @@ library(ggplot2)
 
 # load result data
 # 1 = Yes; 0 = No
-data <- read.csv(file = 'expResults.csv', header = TRUE)
+
+data<-read.csv(file = 'expResults.csv', header = TRUE) #read data 
+
+x<-data[,c("Pie.chart","Bar.chart","Scatter.plot")] #relevant columns
+
+i<-nrow(x) #count number of rows for re-sampling 
+
+boot.mean<-function(x,i){
+  boot.mean<-mean(x[i])} #bootstrapping function to get the mean
+
+#bootstrap all columns
+#gets bootstrap mean and confidence interval 
+apply(x,2,function(y){ 
+  b<-boot(y,boot.mean,R=50000); 
+  c(mean(b$t),boot.ci(b,type="perc", conf=0.95)$percent[4:5])
+})
+
+
+
+
+#Other Attempt
+data<-read.csv(file = 'expResults.csv', header = TRUE) 
 correlation <- function(data, index)
 {
   df <- data[index, ]
   # Find the spear man correlation between the value columns of data set
-  c(cor(df[, 1], df[, 2], df[,3], method = 'spearman'))
+  c(cor(df[, 2], df[,3], method = 'spearman'))
 }
 
 # bootstrap w/ data set and rounds
-bootstrap <- boot(data, correlation, Rounds = 100)
+bootstrap <- boot(data, correlation, R = 100)
 bootstrap
 
 # Plot sampling
